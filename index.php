86b547ba005d17590e096ed12bf0d21384aa2be2
Isabelle <ischmit@u.rochester.edu>
2014-12-04 13:02:04 -0500

    Eko Web Interview Test

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ $page = 'home';
 
 
 		
-		<div class="section team full-panel hidden-xs">
+		<div class="section team full-panel hidden-xs ">
 			<div class="container">
 				<div class="row">
 					
@@ -69,10 +69,51 @@ $page = 'home';
 				</div>
 			</div>
 		</div>
-
+		<div class="section team full-panel visible-xs ">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-sm-7 screenshot-device3">
+					<p>&nbsp;</p>
+		        	<img src="images/2ndpanel.png" class="img-responsive" alt="Responsive image">
+						
+					</div>
+					<div class="col-sm-5 padded-text team2">
+						<h3>It's that simple.</h3>
+						<p>Saying yes or no has never been easier.<br>Add a message if you'd like.</p>
+						<p class="button-row">
+						<p>&nbsp;</p>
+				      <a href="https://itunes.apple.com/us/app/loop-by-eko/id931539244?mt=8", target="itunes_store" button type="button" class="btn btn-default btn-store"> Download For Free</a>
+					</p>
+					</div>
+					
+				</div>
+			</div>
+		</div>
 		
 		<!-- desktop section -->
-		<div class="section stickers full-panel hidden-xs">
+		<div class="section stickers full-panel hidden-xs ">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-1 padded-text"></div>
+					<div class="col-sm-4 padded-text">
+						<h3>Loops are forever.</h3>
+						<p>All Loops are archived for easy access.<br>Never lose track of commitments again.
+							<p>&nbsp;</p>
+							<p class="button-row">
+				      		<a href="https://itunes.apple.com/us/app/loop-by-eko/id931539244?mt=8", target="itunes_store" button type="button" class="btn btn-default btn-store3">Get The App</a>
+							</p>
+					</div>
+
+					<div class="col-sm-7 screenshot-device2">
+						<img src="images/3rdpanel.png" class="img-responsive" alt="Responsive image">
+					</div>
+	
+				</div>
+			</div>
+		</div>
+
+		<div class="section stickers full-panel visible-xs ">
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-1 padded-text"></div>
